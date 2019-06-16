81aab52d87fc021e8fd92eb02bcf001f05947d72
DylanHedges <d.hedges1@hotmail.co.uk>
2019-06-16 12:33:07 -0400

    Added Heroku setup code

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ app.get('/', (req, res) =>{
   res.send({ hi: 'there'});
 });
 
-
-app.listen(5000);
+//Listen on the Heroku environment variable OR port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
