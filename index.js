620bc791c39a389dd2da903ea7228ef39cdb16e8
DylanHedges <d.hedges1@hotmail.co.uk>
2019-06-17 19:42:10 -0400

    Added passport.js and passport google strategy

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) =>{
-  res.send({ hi: 'there'});
-});
+//passport.use() tells app you want to authenticate with a strategy; (new GoogleStrategy()) specifies you want to use Google Passport Strategy (creates new isntance)
+passport.use(new GoogleStrategy());
+
+
 
 //Listen on the Heroku environment variable OR port 5000
 const PORT = process.env.PORT || 5000;
