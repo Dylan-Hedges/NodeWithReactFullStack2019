ef9aadbc41204570f7db5bd575a5c3e8dfac5f9c
DylanHedges <d.hedges1@hotmail.co.uk>
2019-06-18 08:26:53 -0400

    Added Google Strategy credentials & keys

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require ('./config/keys');
+
 const app = express();
 
 //passport.use() tells app you want to authenticate with a strategy; (new GoogleStrategy()) specifies you want to use Google Passport Strategy (creates new isntance)
-passport.use(new GoogleStrategy());
-
-
+passport.use(new GoogleStrategy(
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 //Listen on the Heroku environment variable OR port 5000
 const PORT = process.env.PORT || 5000;
