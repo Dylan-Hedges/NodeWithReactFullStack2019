3a84fd9c274bfb013a02c1c6ed0a261b312ade4a
DylanHedges <d.hedges1@hotmail.co.uk>
2019-07-21 15:31:49 -0400

    Added React-Router, added temp components & routes, added materialize

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
 
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+//BrowserRouter - changes components shown based on URL; Route - sets the rules for what components are shown based on the route the user is on
 const App = () => {
   return(
-    <div>
-      Hi There
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact={true} path="/" component={Landing} />
+          <Route exact={true} path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
