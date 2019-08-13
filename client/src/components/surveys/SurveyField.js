bbbb2f8e651844fe4a6dc12527ac19e1a7a1310a
DylanHedges <d.hedges1@hotmail.co.uk>
2019-08-13 08:17:24 -0400

    Added validation checks for fields, added validateEmail component (checks email list for invalid emails)

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 //Renders a single Field with a label
-export default ({input, label}) => {
+export default ({input, label, meta:{error, touched}}) => {
+  //Pull off label, input, meta(error, touched - displays error on screen if user has touched the field AND there is an error)
   return(
     <div>
       <label>{label}</label>
-      <input {...input}/>
+      <input {...input} style={{marginBottom: '5px'}}/>
+      <div className="red-text" style={{marginBottom: '20px'}}>
+        {touched && error}
+      </div>
     </div>
   );
 };
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form';
 import {Link} from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 //Array for fields - each entry is used to generate a new <Field />
 const FIELDS = [
@@ -34,7 +35,25 @@ class SurveyForm extends Component{
   }
 }
 
-//Wires up reduxForm helper to the component as additonal props (similar to connect)
+//Validation function - performs checks before submitting the form, part of reduxForm, wired up at the bottom (similar to how mapStateToProps is wired up)
+function validate(values){
+  const errors = {};
+  //Checks the recipients list to see if there are any errors - runs the validateEmails function, assigns whatever is returned to errors.emails, || '' pass in empty string on page load
+  errors.emails = validateEmails(values.emails || '');
+  //Adds a property to the errors[name] object containing a string - we use [name] when we reference something on the fly, errors[name] changes for each entry in the array so it will be errors.title then errors.subject etc.
+  _.each(FIELDS, ({name}) => {
+    //If there is no value  - when looking under the name (used as the key for the data in Redux Store)
+    if(!values[name]){
+      //Add a property to the errors object containing a string - Redux Form automatically passes this to the <Field /> with name="title" under meta.error
+      errors[name] = 'You must provide a value';
+    }
+  });
+  //Return errors (If applicable) - if a blank object is returned there are no issues and form submittal can go ahead
+  return errors;
+}
+
+//Wires up reduxForm to the component - provides additonal props (similar to connect)
 export default reduxForm({
+    validate: validate,
     form: 'surveyForm'
 })(SurveyForm);
