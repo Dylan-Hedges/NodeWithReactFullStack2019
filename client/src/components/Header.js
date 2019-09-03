3f3360635f396ee441cea72ad2104eb3e909ad55
DylanHedges <d.hedges1@hotmail.co.uk>
2019-09-03 19:56:28 -0400

    Added logic to prevent user entering create survey page if insufficient credits - menu and side menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,12 +20,32 @@ class Header extends Component{
         );
       //Logged in
       default:
-        return [
-          <li key="1"><Link to={'/surveys/new'} className="newsurveymainbutton"><button className="btn green white-text">New Survey</button></Link></li>,
-          <li key="2"><Payments sideMenu={false} /></li>,
-          <li key="3" style={{margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>,
-          <li key="4"><a href="/api/logout">Logout</a></li>
-        ];
+        //When component first loads auth = null, prevents error
+        if (this.props.auth === null){
+          return <div></div>;
+        }
+        //If the user has enough credits (greater than 0) display the add button to let them create a new survey
+        if(this.props.auth.credits > 0) {
+          return(
+            [
+              <li key="1"><Link to={'/surveys/new'} className="newsurveymainbutton"><button className="btn green white-text">New Survey</button></Link></li>,
+              <li key="2"><Payments sideMenu={false} /></li>,
+              <li key="3" style={{margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>,
+              <li key="4"><a href="/api/logout">Logout</a></li>
+            ]
+          )
+        }
+        //If the user does not have enough credits (less than 0) show a button that when clicked alters the user to add more credits
+        if(this.props.auth.credits <= 0) {
+          return(
+            [
+              <li key="1"><a onClick={() => {alert("Please add more credits to your account before creating a survey.")}} className="newsurveymainbutton"><button className="btn green white-text">New Survey</button></a></li>,
+              <li key="2"><Payments sideMenu={false} /></li>,
+              <li key="3" style={{margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>,
+              <li key="4"><a href="/api/logout">Logout</a></li>
+            ]
+          )
+        }
     }
   }
 
@@ -43,12 +63,32 @@ class Header extends Component{
         );
       //Logged in
       default:
-        return [
-          <li key="5" className="sidemenucredits">Credits: {this.props.auth.credits}</li>,
-          <li key="6"><Link to={'/surveys/new'}>New Survey</Link></li>,
-          <li key="7"><Payments sideMenu={true} /></li>,
-          <li key="8"><a href="/api/logout">Logout</a></li>
-        ];
+        //When component first loads auth = null, prevents error
+        if (this.props.auth === null){
+          return <div></div>;
+        }
+        //If the user has enough credits (greater than 0) display the add button to let them create a new survey
+        if(this.props.auth.credits > 0) {
+          return(
+            [
+              <li key="5" className="sidemenucredits">Credits: {this.props.auth.credits}</li>,
+              <li key="6"><Link to={'/surveys/new'}>New Survey</Link></li>,
+              <li key="7"><Payments sideMenu={true} /></li>,
+              <li key="8"><a href="/api/logout">Logout</a></li>
+            ]
+          )
+        }
+        //If the user does not have enough credits (less than 0) show a button that when clicked alters the user to add more credits
+        if(this.props.auth.credits <= 0) {
+          return(
+            [
+              <li key="5" className="sidemenucredits">Credits: {this.props.auth.credits}</li>,
+              <li key="6"><a onClick={() => {alert("Please add more credits to your account before creating a survey.")}}>New Survey</a></li>,
+              <li key="7"><Payments sideMenu={true} /></li>,
+              <li key="8"><a href="/api/logout">Logout</a></li>
+            ]
+          )
+        }
     }
   }
 
