112cae16091a4e4444fbbf7b14aa36619b67788b
DylanHedges <d.hedges1@hotmail.co.uk>
2019-09-12 19:11:12 -0400

    Added last responded date to sent surveys

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,6 +8,25 @@ class SurveyList extends Component {
   componentDidMount(){
     this.props.fetchSurveys();
   }
+  //Function that checks if last responded date is invalid & returns JSX
+  displayLastResponded(survey){
+    //Pulls date object from the DB and converts it to a readble string
+    const date = new Date(survey.lastResponded).toLocaleDateString();
+    //Checks if date once converted is "Invalid Date" (there are no responses yet from recipients)
+    if( date === "Invalid Date"){
+      return(
+        <p className="right">
+          Last Responded: No responses yet
+        </p>
+      )
+    }
+    //Returns JSX with date of last response
+    return(
+      <p className="right">
+        Last Responded: {date}
+      </p>
+    )
+  }
   //Renders the surveys on screen - maps over the surveys prop (list of surveys) and generates JSX for each survey
   renderSurveys(){
     return this.props.surveys.reverse().map(survey => {
@@ -24,6 +43,7 @@ class SurveyList extends Component {
             <a className="green-text">Yes:<span className="response">{survey.yes}</span></a>
             <a className="green-text">No:<span className="response">{survey.no}</span></a>
             <button className="red darken-1 btn-flat white-text" onClick={() => this.props.deleteSurvey({surveyid:survey._id})}>Delete</button>
+            {this.displayLastResponded(survey)}
           </div>
         </div>
       )
